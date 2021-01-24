dae93c326087196829b477934e8b2b68fb2168cd
adrict99 <adriancastrotorres9@gmail.com>
2021-01-24 14:30:09 +0100

    Initial commit, everything is working, need to handle multiple exceptions, fix squareroots and calc history

---
+++ app/src/main/java/com/adrict99/calculator/MainActivity.java
@@ -0,0 +1,154 @@
+package com.adrict99.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.Math;
+
+public class MainActivity extends AppCompatActivity {
+
+    float mFirst, mSecond;
+
+    String operation;
+    String historyStorage;
+    int historyCounter = 0;
+    double result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonPlus = findViewById(R.id.btnPlus);
+        final Button buttonTimes = findViewById(R.id.btnTimes);
+        final Button buttonDiv = findViewById(R.id.btnDiv);
+        final Button buttonEqual = findViewById(R.id.btnEqual);
+        final Button buttonMinus = findViewById(R.id.btnMinus);
+        final Button buttonPercentage = findViewById(R.id.btnPerc);
+        final Button buttonRaise = findViewById(R.id.btnElev);
+        final Button buttonSquareRoot = findViewById(R.id.btnPosNeg);
+        final Button btn1 = findViewById(R.id.num1);
+        final Button btn2 = findViewById(R.id.num2);
+        final Button btn3 = findViewById(R.id.num3);
+        final Button btn4 = findViewById(R.id.num4);
+        final Button btn5 = findViewById(R.id.num5);
+        final Button btn6 = findViewById(R.id.num6);
+        final Button btn7 = findViewById(R.id.num7);
+        final Button btn8 = findViewById(R.id.num8);
+        final Button btn9 = findViewById(R.id.num9);
+        final Button btn0 = findViewById(R.id.num0);
+        final Button btnComma = findViewById(R.id.btnComma);
+        final Button btnCE = findViewById(R.id.btnCE);
+        final TextView historyOne = findViewById(R.id.historyOne);
+        final TextView historyTwo = findViewById(R.id.historyTwo);
+        final EditText operationNumbers = findViewById(R.id.resultEditText);
+
+        operationNumbers.setInputType(InputType.TYPE_NULL);
+
+        btn1.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "1"));
+        btn2.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "2"));
+        btn3.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "3"));
+        btn4.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "4"));
+        btn5.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "5"));
+        btn6.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "6"));
+        btn7.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "7"));
+        btn8.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "8"));
+        btn9.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "9"));
+        btn0.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "0"));
+        btnComma.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "."));
+        btnCE.setOnClickListener(v -> {
+            operationNumbers.setText("");
+
+        });
+        //TODO: Change theme light to dark and viceversa on onClick
+        //TODO: Style history operations and maybe add another one
+        //TODO: Add more functionalities
+        buttonPlus.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "Plus";
+            operationNumbers.setText(null);
+        });
+        buttonMinus.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "Minus";
+            operationNumbers.setText(null);
+        });
+        buttonTimes.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "Times";
+            operationNumbers.setText(null);
+        });
+        buttonDiv.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "Div";
+            operationNumbers.setText(null);
+        });
+        buttonPercentage.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "Percentage";
+            operationNumbers.setText(null);
+        });
+        buttonRaise.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "Raise";
+            operationNumbers.setText(null);
+        });
+        buttonSquareRoot.setOnClickListener(v -> {
+            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            operation = "SquareR";
+            operationNumbers.setText(null);
+        });
+        buttonEqual.setOnClickListener(v -> {
+                switch (operation){
+                    case "Plus" :
+                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        Float resultPlus = mFirst + mSecond;
+                        operationNumbers.setText(resultPlus.toString());
+                        historyOne.setText(mFirst + " " + "+" + " " + mSecond + " = " + resultPlus);
+                        break;
+                    case "Minus" :
+                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        Float resultMinus = mFirst - mSecond;
+                        operationNumbers.setText(resultMinus.toString());
+                        historyOne.setText(mFirst + " " + "-" + " " + mSecond + " = " + resultMinus);
+                        break;
+                    case "Times" :
+                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        Float resultTimes = mFirst * mSecond;
+                        operationNumbers.setText(resultTimes.toString());
+                        historyOne.setText(mFirst + " " + "*" + " " + mSecond + " = " + resultTimes);
+                        break;
+                    case "Div" :
+                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        Float resultDiv = mFirst / mSecond;
+                        operationNumbers.setText(resultDiv.toString());
+                        historyOne.setText(mFirst + " " + "/" + " " + mSecond + " = " + resultDiv);
+                        break;
+                    case "Percentage" :
+                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        Float resultPercentage = (mFirst / 100) * mSecond;
+                        operationNumbers.setText(resultPercentage.toString());
+                        historyOne.setText(mFirst + " " + "%" + " " + mSecond + " = " + resultPercentage);
+                        break;
+                    case "Raise" :
+                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        double resultRaise = Math.pow(mFirst, mSecond);
+                        operationNumbers.setText(String.valueOf(resultRaise));
+                        historyOne.setText(mFirst + " " + "ₓⁿ" + " " + mSecond + " = " + resultRaise);
+                        break;
+                    case "SquareR" :
+                        double resultSquareR = Math.sqrt(mFirst);
+                        String res = String.valueOf(resultSquareR);
+                        operationNumbers.setText(res);
+                        historyOne.setText(mFirst + " " + "ⁿ√ₓ" + " " + mSecond + " = " + resultSquareR);
+                        break;
+                }
+        });
+    }
+}
