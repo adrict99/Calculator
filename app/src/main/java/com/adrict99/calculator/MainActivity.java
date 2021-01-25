95f5b9ef95dd54cda48817e9914d8aefc9a25241
adrict99 <adriancastrotorres9@gmail.com>
2021-01-25 10:19:47 +0100

    Changed variables to Double, fixed Square Root to do square roots and limited max lenght on the EditText to 7

--- app/src/main/java/com/adrict99/calculator/MainActivity.java
+++ app/src/main/java/com/adrict99/calculator/MainActivity.java
@@ -3,7 +3,6 @@ package com.adrict99.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +12,10 @@ import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
 
-    float mFirst, mSecond;
+    double mFirst;
+    double mSecond;
 
     String operation;
-    String historyStorage;
-    int historyCounter = 0;
-    double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
         final Button buttonEqual = findViewById(R.id.btnEqual);
         final Button buttonMinus = findViewById(R.id.btnMinus);
         final Button buttonPercentage = findViewById(R.id.btnPerc);
-        final Button buttonRaise = findViewById(R.id.btnElev);
-        final Button buttonSquareRoot = findViewById(R.id.btnPosNeg);
+        final Button buttonRaise = findViewById(R.id.btnRaise);
+        final Button buttonSquareRoot = findViewById(R.id.btnRoot);
         final Button btn1 = findViewById(R.id.num1);
         final Button btn2 = findViewById(R.id.num2);
         final Button btn3 = findViewById(R.id.num3);
@@ -45,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         final Button btn0 = findViewById(R.id.num0);
         final Button btnComma = findViewById(R.id.btnComma);
         final Button btnCE = findViewById(R.id.btnCE);
-        final TextView historyOne = findViewById(R.id.historyOne);
-        final TextView historyTwo = findViewById(R.id.historyTwo);
+        final TextView historyOne = findViewById(R.id.historyTextView);
+        final TextView historyTwo = findViewById(R.id.history2TextView);
         final EditText operationNumbers = findViewById(R.id.resultEditText);
 
         operationNumbers.setInputType(InputType.TYPE_NULL);
@@ -64,80 +61,79 @@ public class MainActivity extends AppCompatActivity {
         btnComma.setOnClickListener(v -> operationNumbers.setText(operationNumbers.getText() + "."));
         btnCE.setOnClickListener(v -> {
             operationNumbers.setText("");
-
         });
         //TODO: Change theme light to dark and viceversa on onClick
         //TODO: Style history operations and maybe add another one
         //TODO: Add more functionalities
         buttonPlus.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "Plus";
             operationNumbers.setText(null);
         });
         buttonMinus.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "Minus";
             operationNumbers.setText(null);
         });
         buttonTimes.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "Times";
             operationNumbers.setText(null);
         });
         buttonDiv.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "Div";
             operationNumbers.setText(null);
         });
         buttonPercentage.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "Percentage";
             operationNumbers.setText(null);
         });
         buttonRaise.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "Raise";
             operationNumbers.setText(null);
         });
         buttonSquareRoot.setOnClickListener(v -> {
-            mFirst = Float.parseFloat(operationNumbers.getText() + "");
+            mFirst = Double.parseDouble(String.valueOf(operationNumbers.getText()));
             operation = "SquareR";
             operationNumbers.setText(null);
         });
         buttonEqual.setOnClickListener(v -> {
                 switch (operation){
                     case "Plus" :
-                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
-                        Float resultPlus = mFirst + mSecond;
+                        mSecond = Double.parseDouble(String.valueOf(operationNumbers.getText()));
+                        Double resultPlus = mFirst + mSecond;
                         operationNumbers.setText(resultPlus.toString());
                         historyOne.setText(mFirst + " " + "+" + " " + mSecond + " = " + resultPlus);
                         break;
                     case "Minus" :
-                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
-                        Float resultMinus = mFirst - mSecond;
+                        mSecond = Double.parseDouble(String.valueOf(operationNumbers.getText()));
+                        Double resultMinus = mFirst - mSecond;
                         operationNumbers.setText(resultMinus.toString());
                         historyOne.setText(mFirst + " " + "-" + " " + mSecond + " = " + resultMinus);
                         break;
                     case "Times" :
-                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
-                        Float resultTimes = mFirst * mSecond;
+                        mSecond = Double.parseDouble(String.valueOf(operationNumbers.getText()));
+                        Double resultTimes = mFirst * mSecond;
                         operationNumbers.setText(resultTimes.toString());
                         historyOne.setText(mFirst + " " + "*" + " " + mSecond + " = " + resultTimes);
                         break;
                     case "Div" :
-                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
-                        Float resultDiv = mFirst / mSecond;
+                        mSecond = Double.parseDouble(String.valueOf(operationNumbers.getText()));
+                        Double resultDiv = mFirst / mSecond;
                         operationNumbers.setText(resultDiv.toString());
                         historyOne.setText(mFirst + " " + "/" + " " + mSecond + " = " + resultDiv);
                         break;
                     case "Percentage" :
-                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
-                        Float resultPercentage = (mFirst / 100) * mSecond;
+                        mSecond = Double.parseDouble(String.valueOf(operationNumbers.getText()));
+                        Double resultPercentage = (mFirst / 100) * mSecond;
                         operationNumbers.setText(resultPercentage.toString());
                         historyOne.setText(mFirst + " " + "%" + " " + mSecond + " = " + resultPercentage);
                         break;
                     case "Raise" :
-                        mSecond = Float.parseFloat(String.valueOf(operationNumbers.getText()));
+                        mSecond = Double.parseDouble(String.valueOf(operationNumbers.getText()));
                         double resultRaise = Math.pow(mFirst, mSecond);
                         operationNumbers.setText(String.valueOf(resultRaise));
                         historyOne.setText(mFirst + " " + "ₓⁿ" + " " + mSecond + " = " + resultRaise);
@@ -146,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
                         double resultSquareR = Math.sqrt(mFirst);
                         String res = String.valueOf(resultSquareR);
                         operationNumbers.setText(res);
-                        historyOne.setText(mFirst + " " + "ⁿ√ₓ" + " " + mSecond + " = " + resultSquareR);
+                        historyOne.setText(mFirst + " " + "ⁿ√ₓ" + " = " + resultSquareR);
                         break;
                 }
         });
